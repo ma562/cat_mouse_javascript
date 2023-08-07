c321a507b03ccc21b960e3692b392c69f783cad7
Joseph Ma <Josephma@pal-10-184-98-126.itap.purdue.edu>
2023-08-07 10:12:57 -0400

    testing gyro.js

--- index.js
+++ index.js
@@ -1303,26 +1303,11 @@ window.addEventListener('keyup', ({key}) => {
 	}
 })
 
-if ('ondeviceorientation' in window) {
-    // Add an event listener for the 'deviceorientation' event
-    window.addEventListener('deviceorientation', handleOrientation);
-    alert("it has been done")
-} else {
-    alert("Device orientation not supported by this browser.");
-}
-
-// Add an event listener for the 'deviceorientation' event
-// window.addEventListener('deviceorientation', handleOrientation, true);
-alert("now testing");
-// Function to handle device orientation changes
-function handleOrientation(event) {
-    alert("has been called");
-    // Display specific orientation data using alerts
-    alert('Alpha: ' + event.alpha + '\nBeta: ' + event.beta + '\nGamma: ' + event.gamma);
-}
-
-
-
+gyro.startTracking(function(o) {
+    // o.x, o.y, o.z for accelerometer
+    // o.alpha, o.beta, o.gamma for gyro
+    alert(o.alpha);
+  });
 
 
 
